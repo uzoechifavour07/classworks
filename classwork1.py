bb1ff19566c4a3e94514476ce6d945adc64cbefc
favour <uzoechifavour07@gmail.com>
2025-12-20 21:59:45 +0000

    no function

--- classwork1.py
+++ classwork1.py
@@ -1,27 +1,7 @@
-'''name = input("what is your name?")
-balance = input("what is your account balance ?")
-
-print (f" hi my name is {name}, and i have {balance} in my wallet address")
-'''
-
-
-def addtobalance ():
-       account_balance = 5000
-       topup = int(input("how huch do you want to deposit ? "))
-       global current
-       current= account_balance + topup
-       
-       print(f"your account balance is {current}")
-
-addtobalance()
-
-
-def subbalance():
-       
-       amount = int(input("how much do you wan to withdraw ? "))
-       balance = current - amount
-
-       print(f"your currrent balance is {balance}")
-
-subbalance()
-    
+balance = 5000
+topup = int(input("how much you you want to deposit ? "))
+current = balance + topup
+print(f"your current balance is {current}")
+remove_balance = int(input("how much do you want to withdraw"))
+current = current - remove_balance
+print(f"your current balance is {current}. ")
